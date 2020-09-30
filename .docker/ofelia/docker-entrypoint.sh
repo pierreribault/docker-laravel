51a521eb016944ba186edc880b45077cd82207a7
Pierre Ribault <contact@pierreribault.fr>
2020-09-30 19:53:30 +0200

    :tada: Init the repository

---
+++ .docker/ofelia/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script start the project Clients V2
+
+# Waiting dependancies
+/wait
+
+# Start Ofelia
+/usr/bin/ofelia daemon --docker
---
+++ README.md
@@ -0,0 +1,1 @@
+👋🏻  Docker + Lando + Laravel
